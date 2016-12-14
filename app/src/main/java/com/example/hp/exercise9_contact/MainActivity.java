0bc742607dc8d002fc3eedae039a7f13c822c6c1
hp-PC\hp <Mahdi.heravi@gmail.com>
2016-12-14 13:39:30 +0330

    import android.provider.ContactsContract; to minActivity

--- app/src/main/java/com/example/hp/exercise9_contact/MainActivity.java
+++ app/src/main/java/com/example/hp/exercise9_contact/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.hp.exercise9_contact;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+
 
 public class MainActivity extends AppCompatActivity {
 
